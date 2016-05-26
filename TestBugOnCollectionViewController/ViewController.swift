49f42524decb0c9b003e3faa5ad737d714ad8dcb
Yitschak Ben ezra <itsikbe@gmail.com>
2016-05-26 15:38:05 +0300

    Init a horizontal collection view controller

--- TestBugOnCollectionViewController/ViewController.swift
+++ TestBugOnCollectionViewController/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView?.scrollToItemAtIndexPath(NSIndexPath(forRow: 4, inSection: 3), atScrollPosition: .None, animated: false)
     }
-
 
 }
 
+//MARK: DataSource implementation
+extension ViewController {
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let newCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
+        
+        if let myCell = newCell as? MyCell {
+            myCell.label.text = "\(indexPath.section):\(indexPath.item.description)"
+        }
+        
+        return newCell
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 10
+    }
+}
+
+class MyCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
