b649074d7100769cf03f77570cd39cb97dccfa61
Yitschak Ben ezra <itsikbe@gmail.com>
2016-05-26 14:33:51 +0300

    Initial Commit

---
+++ TestBugOnCollectionViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestBugOnCollectionViewController
+//
+//  Created by יצחק בן-עזרא on 26.5.2016.
+//  Copyright © 2016 יצחק בן-עזרא. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
