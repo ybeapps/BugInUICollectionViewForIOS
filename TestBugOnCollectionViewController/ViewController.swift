cc153ee149de086cbfd7cb9b080cd09e48639825
יצחק בן-עזרא <ybeapps@gmail.com>
2017-07-20 21:01:13 +0300

    update to latest SDK. iOS 11, XCode Version 9.0 beta 3 (9M174d) the bug is worse now!!

--- TestBugOnCollectionViewController/ViewController.swift
+++ TestBugOnCollectionViewController/ViewController.swift
@@ -10,41 +10,90 @@ import UIKit
 
 class ViewController: UICollectionViewController {
 
+    @available(iOS 10.0, *)
+    func callWithDelay(_ seconds: Double, action: @escaping ()->()) {
+        let delay = seconds * Double(NSEC_PER_SEC)
+        let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            action()
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: seconds, repeats: false) { (timer) in
+            action()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        collectionView?.scrollToItemAtIndexPath(NSIndexPath(forRow: 4, inSection: 3), atScrollPosition: .None, animated: false)
+        let target = IndexPath(row: 1, section: 1)
+        
+        let scroll = {
+            self.collectionView?.scrollToItem(at: target, at: UICollectionViewScrollPosition.centeredHorizontally, animated: false)
+            if let layout = self.collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+                print ("updating layout to big cell")
+                layout.itemSize = self.collectionView!.frame.size
+            }
+        }
+        
+        if #available(iOS 10.0, *) {
+            callWithDelay(10) {
+                scroll()
+            }
+        } else {
+            // Fallback on earlier versions
+            scroll()
+        }
+//        collectionView?.scrollToItem(at: IndexPath(row: 4, section: 3), at: [], animated: false)
     }
 
 }
 
 //MARK: DataSource implementation
 extension ViewController {
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 30
     }
     
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let newCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath)
+        let newCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
         
         if let myCell = newCell as? MyCell {
-            myCell.label.text = "\(indexPath.section):\(indexPath.item.description)"
+            myCell.label.text = "section \(indexPath.section) : item \(indexPath.item.description)"
         }
         
         return newCell
     }
     
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 10
     }
 }
 
 class MyCell: UICollectionViewCell {
     @IBOutlet weak var label: UILabel!
+    
+    func rand() -> Float {
+        return Float((arc4random() % 100)) / 100
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.init(colorLiteralRed: rand(), green: rand(), blue: rand(), alpha: 1)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = UIColor.init(colorLiteralRed: rand(), green: rand(), blue: rand(), alpha: 1)
+    }
 }
